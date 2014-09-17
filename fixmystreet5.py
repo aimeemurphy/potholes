8898351b6558ed276315f916398d3d6eac8e2e2f
aimeemurphy <asjmurphy@gmail.com>
2014-09-17 15:03:43 +0100

    removes string around the council name

--- fixmystreet5.py
+++ fixmystreet5.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from pyquery import PyQuery as pq
 from HTMLParser import HTMLParser
 import time
+import re
 
 #compile url of url start + counter padded with zeros using zfill
 print "Enter report ID to start from: \n Note: The first ID is 10, or start from the most recent hazardid already obtained"
@@ -12,10 +13,10 @@ counter = int(raw_input())
 #get lastreportid by getting the url of the most recent report on main page
 #and clipping off last six digits of url as the report id
 
-mainpage_url=("http://www.fixmystreet.com")
-r=requests.get(mainpage_url)
-webpage=r.text
-soup=BeautifulSoup(webpage)
+mainpage_url = ("http://www.fixmystreet.com")
+r = requests.get(mainpage_url)
+webpage = r.text
+soup = BeautifulSoup(webpage)
 
 #this will extract URLs through a loop, add each URL to a list
 soup_out = ['Soup output',]
@@ -33,7 +34,7 @@ print "The most recent hazard report ID is ID %r at link %r" % (lastreportID, so
 #ee ex16 for making csv 'a' append 'wb' open as empty file and start writing
 target = open('hazard_reports1.csv', 'wb')
 b = csv.writer(target)
-data = ['currentreportID', 'report_url', 'report_title']
+data = ['currentreportID', 'report_url', 'report_title', 'council', 'description']
 b.writerow(data)
 
 #while loop to keep looping until we reach the ID of the last reported hazard
@@ -47,13 +48,34 @@ while counter < lastreportID:
 	response = requests.get(report_url)
 
 	#.text() prints the text only, without html tags, we know h1 is the title from inspection
-	title = pq(response.content)
-	print 'title:', title('h1').text()
-	print currentreportid
+	content = pq(response.content)
 
 	#add to report titles csv
-	report_title = title('h1').text()
-	data = [currentreportid, report_url, report_title]
+	report_title = content('h1').text()
+	#searching for tag.class on the page and pulls text only, need to search text to pick out council later
+	council = content("small.council_sent_info").text()
+	council = council.replace("Sent to ", "").replace("later", "")
+	print council
+	#find where digits start appearing, i.e., the time that we want to cut off
+	x = re.search("\d", council)
+	print x
+	p = re.compile("\d")
+	for m in p.finditer(council):
+		a = m.start()
+		print a
+		council = council[:a-1]
+	print council
+
+
+	#category is within a <p> tag with date and text we don't want. 
+	#can use replace to replace the standard text with empty text
+	#will probably need to use "regular expressions" to look for where the date starts etc
+	#category = content()
+
+	description = content("div.problem-header div.moderate-display p").text()
+	print description
+
+	data = [currentreportid, report_url, report_title, council, description]
 	print data
 	b.writerow(data)
 
